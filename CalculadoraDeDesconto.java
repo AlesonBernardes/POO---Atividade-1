b651073aaa14746969ed4ecd30acb68261de071e
AlesonBernardes <166345319+AlesonBernardes@users.noreply.github.com>
2024-06-18 10:24:00 -0300

    Calculadora de Desconto - 2

--- CalculadoraDeDesconto.java
+++ CalculadoraDeDesconto.java
@@ -4,17 +4,17 @@ public class CalculadoraDeDesconto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o valor do produto: ");
+        System.out.print("Digite o valor do produto: R$ ");
         double valorProduto = scanner.nextDouble();
 
-        System.out.print("Digite a porcentagem de desconto: ");
+        System.out.print("Digite a porcentagem de desconto: R$ ");
         double porcentagemDesconto = scanner.nextDouble();
 
         double valorDesconto = valorProduto * (porcentagemDesconto / 100);
         double precoFinal = valorProduto - valorDesconto;
 
-        System.out.printf("Valor do desconto: %.2f%n", valorDesconto);
-        System.out.printf("Preço final do produto: %.2f%n", precoFinal);
+        System.out.printf("Valor do desconto: R$ %.2f%n", valorDesconto);
+        System.out.printf("Preço final do produto: R$ %.2f%n", precoFinal);
 
         scanner.close();
     }
